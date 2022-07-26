35e149ef4385247a1921873f2ad47e53d68a70f7
Matthew Mickler <matthewmickler@Matthews-MBP.hitronhub.home>
2022-07-26 15:15:05 -0700

    restful commit

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -1,9 +1,13 @@
-import morgan from "morgan";
+
+import bodyParser from "body-parser";
 import express from "express";
 import {readFile} from "fs/promises";
 import {writeFile} from "fs/promises";
+import morgan from "morgan";
 const app = express();
 app.use(express.json());
+// app.use(morgan());
+// app.use(bodyParser());
 const errorHandler1 = (req, res, next) => {
     res.sendStatus(404)
     next();
@@ -39,6 +43,7 @@ app.post("/pets", (req, res, next) => {
         if(typeof newPet.age === 'number' && newPet.kind && newPet.name){
             existingPets.push(newPet);
             return writeFile("pets.json", JSON.stringify(existingPets)).then( () =>{
+                res.sendStatus(201);
                 res.send('pets push');
             });
         } else {
@@ -46,6 +51,48 @@ app.post("/pets", (req, res, next) => {
         }
     }).catch(next)
 });
+
+app.patch("/pets/:id", (req, res, next) => {
+    const index = req.params.id;
+    const newPet = req.body;
+    newPet.age = parseInt(newPet.age);
+    readFile("pets.json", "utf-8").then(str => {
+        const existingPets = JSON.parse(str);
+            if(newPet.age) {
+                existingPets[index].age = newPet.age;
+            }
+            else if(newPet.kind){
+                existingPets[index].kind = newPet.kind;
+            }
+            else if(newPet.name){
+                existingPets[index].name = newPet.name;
+            }
+            else {
+                res.sendStatus(400);
+            }    
+            return writeFile("pets.json", JSON.stringify(existingPets)).then( () =>{
+                res.sendStatus(200);
+                res.send('pets patch');
+            });
+
+    }).catch(next)
+});
+
+app.delete("/pets/:id", (req, res, next) => {
+    const index = req.params.id;
+    readFile("pets.json", "utf-8").then(str => {
+        const existingPets = JSON.parse(str);
+        if(existingPets[index]){
+            existingPets.splice(index, 1);
+            return writeFile("pets.json", JSON.stringify(existingPets)).then( () =>{
+                res.sendStatus(200);
+                res.send('pets delete');
+            });
+        } else {
+            res.sendStatus(404);
+        }
+    }).catch(next)
+})
 
 app.use(errorHandler1);
 app.use(function (err, req, res, next) {
